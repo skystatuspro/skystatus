e4d457e0bb5cf901ab8a4deccb991211d11daf0d
skystatuspro <remcodegraaf@gmail.com>
2025-12-14 13:27:04 +0100

    Remove accidentally committed subfolder

--- skystatus-main/src/components/MilesEngine/helpers.tsx
+++
@@ -1,4 +0,0 @@
-// src/components/MilesEngine/helpers.tsx
-// Helper functions for MilesEngine
-
-export const noSpinnerClass = "[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none";
