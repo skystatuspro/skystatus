91c8fd8aad0170532668711478cd5354aaead34e
skystatuspro <remcodegraaf@gmail.com>
2025-12-19 21:01:08 +0100

    Bump version to 2.5.0

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,7 +1,7 @@
-/**
+5/**
  * App Version
  * 
  * ⚠️  UPDATE THIS when releasing a new version!
  *     Also update "version" in package.json to match.
  */
-export const APP_VERSION = '2.4.0';
+export const APP_VERSION = '2.5.0';
